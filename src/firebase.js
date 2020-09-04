5f7584853636cb9f9e029605fb248e00f380d1a9
riley <riley.stahura@gmail.com>
2020-09-04 13:34:26 -0600

    env config

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase"
 
 const config = {
-    apiKey: "AIzaSyDAEifQSP26-aT1IUC9fFCPvaOlaFJhlwA",
-    authDomain: "school-check-in-10062.firebaseapp.com",
-    databaseURL: "https://school-check-in-10062.firebaseio.com",
-    projectId: "school-check-in-10062",
-    storageBucket: "school-check-in-10062.appspot.com",
-    messagingSenderId: "428077651619",
-    appId: "1:428077651619:web:639f7ad048f1f0d9b99116",
-    measurementId: "G-73VG1KWBD9"
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
 }
 
 
